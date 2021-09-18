50d7ee88e5a3c6271255e844161f0cadb57d163f
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-09-18 19:23:14 +0900

    websocket added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, Method } from 'axios';
 import moment from 'moment';
 import crypto from 'crypto';
 import qs from 'qs';
 import _ from 'lodash';
+import WebSocket from 'ws';
+import { setWsHeartbeat } from 'ws-heartbeat/client';
 
-type RequestMethod = 'get' | 'post' | 'put' | 'delete';
+interface Websocket extends WebSocket {
+	_events?: object;
+}
+
 type OrderSide = 'buy' | 'sell';
 type OrderType = 'limit' | 'market';
 
@@ -36,7 +41,7 @@ interface AuthHeaders {
 }
 
 interface RequestConfig {
-	method: RequestMethod;
+	method: Method | 'connect';
 	path: string;
 	params?: object;
 	data?: object;
@@ -47,6 +52,7 @@ interface ClientConfig {
 	apiKey?: string;
 	secret?: string;
 	expires?: number;
+	version?: number;
 }
 
 interface BasicQueryParams {
@@ -80,12 +86,24 @@ interface OrdersQueryParmas extends BasicQueryParams {
 	open?: boolean;
 }
 
+interface CreateSignatureOpts {
+	data?: object;
+	params?: object;
+}
+
 export default class Client {
 	private readonly apiUrl: string;
 	private readonly apiKey: string;
 	private readonly secret: string;
 	private readonly expires: number;
 	private readonly instance: AxiosInstance;
+	ws?: Websocket;
+	private readonly wsUrl: string;
+	private wsEvents: string[];
+	private wsReconnect: boolean;
+	private wsReconnectInterval: number;
+	private wsEventListeners?: object;
+	private initialConnection?: boolean;
 
 	constructor(params: ClientConfig) {
 		this.apiUrl = params.apiUrl || 'https://api.hollaex.com/v2';
@@ -95,34 +113,51 @@ export default class Client {
 		this.instance = axios.create({
 			baseURL: this.apiUrl
 		});
+		this.wsUrl = `wss://${this.apiUrl.split('/')[2]}/stream`;
+		this.wsEvents = [];
+		this.wsReconnect = true;
+		this.wsReconnectInterval = 5000;
 	}
 
 	private getFullUrl(path: string, params?: object): string {
 		if (params) {
 			params = _.mapKeys(params, (value, key) => _.snakeCase(key));
 		}
-		return `${this.apiUrl}${path}${
+		return `${path.includes('/stream') ? this.wsUrl : this.apiUrl}${path}${
 			params ? qs.stringify(params, { addQueryPrefix: true }) : ''
 		}`;
 	}
 
-	private generateAuthHeaders(requestData: RequestConfig): AuthHeaders {
-		const expires = moment().unix() + this.expires;
-		const { method, path, params, data } = requestData;
+	private createHmacSignature(
+		method: Method | 'connect',
+		path: string,
+		expires: number,
+		opts?: CreateSignatureOpts
+	) {
 		const formattedString = `${method.toUpperCase()}${this.getFullUrl(
 			path,
-			params
-		)}${expires}${data ? JSON.stringify(data) : ''}`;
+			opts?.params
+		)}${expires}${opts?.data ? JSON.stringify(opts.data) : ''}`;
 
 		const signature = crypto
 			.createHmac('sha256', this.secret)
 			.update(formattedString)
 			.digest('hex');
 
+		return signature;
+	}
+
+	private generateAuthHeaders(requestData: RequestConfig): AuthHeaders {
+		const expires = moment().unix() + this.expires;
+		const { method, path, params, data } = requestData;
+
 		const headers = {
 			'api-key': this.apiKey,
 			'api-expires': expires,
-			'api-signature': signature
+			'api-signature': this.createHmacSignature(method, path, expires, {
+				params,
+				data
+			})
 		};
 
 		return headers;
@@ -380,5 +415,204 @@ export default class Client {
 			})
 		});
 		return data;
+	}
+
+	wsConnected() {
+		return this.ws && this.ws.readyState === WebSocket.OPEN;
+	}
+
+	wsConnect(events: string[]) {
+		this.initialConnection = true;
+		this.wsReconnect = true;
+		this.wsEvents = events;
+		let url = this.wsUrl;
+		if (this.apiKey && this.secret) {
+			url = `${url}?${qs.stringify(
+				this.generateAuthHeaders({
+					method: 'connect',
+					path: '/stream'
+				}),
+				{ addQueryPrefix: true }
+			)}`;
+		}
+		this.ws = new WebSocket(url);
+
+		this.ws.on('unexpected-response', () => {
+			if (this.ws?.readyState !== WebSocket.CLOSING) {
+				if (this.ws?.readyState === WebSocket.OPEN) {
+					this.ws.close();
+				} else if (this.wsReconnect) {
+					this.wsEventListeners = this.ws?._events;
+					this.ws = undefined;
+					setTimeout(() => {
+						this.wsConnect(this.wsEvents);
+					}, this.wsReconnectInterval);
+				} else {
+					this.wsEventListeners = undefined;
+					this.ws = undefined;
+				}
+			}
+		});
+
+		this.ws.on('error', () => {
+			if (this.ws?.readyState !== WebSocket.CLOSING) {
+				if (this.ws?.readyState === WebSocket.OPEN) {
+					this.ws.close();
+				} else if (this.wsReconnect) {
+					this.wsEventListeners = this.ws?._events;
+					this.ws = undefined;
+					setTimeout(() => {
+						this.wsConnect(this.wsEvents);
+					}, this.wsReconnectInterval);
+				} else {
+					this.wsEventListeners = undefined;
+					this.ws = undefined;
+				}
+			}
+		});
+
+		this.ws.on('close', () => {
+			if (this.wsReconnect) {
+				this.wsEventListeners = this.ws?._events;
+				this.ws = undefined;
+				setTimeout(() => {
+					this.wsConnect(this.wsEvents);
+				}, this.wsReconnectInterval);
+			} else {
+				this.wsEventListeners = undefined;
+				this.ws = undefined;
+			}
+		});
+
+		this.ws.on('open', () => {
+			if (this.wsEvents.length > 0) {
+				this.subscribe(this.wsEvents);
+			}
+
+			this.initialConnection = false;
+			// @ts-ignore
+			setWsHeartbeat(this.ws, JSON.stringify({ op: 'ping' }), {
+				pingTimeout: 60000,
+				pingInterval: 25000
+			});
+		});
+	}
+
+	disconnect() {
+		if (this.wsConnected()) {
+			this.wsReconnect = false;
+			this.ws?.close();
+		} else {
+			throw new Error('Websocket not connected');
+		}
+	}
+
+	subscribe(events: string[] = []) {
+		if (this.wsConnected()) {
+			for (const event of events) {
+				if (!this.wsEvents.includes(event) || this.initialConnection) {
+					const [topic, symbol] = event.split(':');
+					switch (topic) {
+						case 'orderbook':
+						case 'trade':
+							if (symbol) {
+								if (!this.wsEvents.includes(topic)) {
+									this.ws?.send(
+										JSON.stringify({
+											op: 'subscribe',
+											args: [`${topic}:${symbol}`]
+										})
+									);
+									if (!this.initialConnection) {
+										this.wsEvents = _.union(this.wsEvents, [
+											event
+										]);
+									}
+								}
+							} else {
+								this.ws?.send(
+									JSON.stringify({
+										op: 'subscribe',
+										args: [topic]
+									})
+								);
+								if (!this.initialConnection) {
+									this.wsEvents = _.union(this.wsEvents, [
+										event
+									]);
+								}
+							}
+							break;
+						case 'order':
+						case 'wallet':
+						case 'deposit':
+							this.ws?.send(
+								JSON.stringify({
+									op: 'subscribe',
+									args: [topic]
+								})
+							);
+							if (!this.initialConnection) {
+								this.wsEvents = _.union(this.wsEvents, [event]);
+							}
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		} else {
+			throw new Error('Websocket not connected');
+		}
+	}
+
+	unsubscribe(events: string[] = []) {
+		if (this.wsConnected()) {
+			for (const event of events) {
+				if (this.wsEvents.includes(event)) {
+					const [topic, symbol] = event.split(':');
+					switch (topic) {
+						case 'orderbook':
+						case 'trade':
+							if (symbol) {
+								this.ws?.send(
+									JSON.stringify({
+										op: 'unsubscribe',
+										args: [`${topic}:${symbol}`]
+									})
+								);
+							} else {
+								this.ws?.send(
+									JSON.stringify({
+										op: 'unsubscribe',
+										args: [topic]
+									})
+								);
+							}
+							this.wsEvents = this.wsEvents.filter(
+								(e) => e !== event
+							);
+							break;
+						case 'order':
+						case 'wallet':
+						case 'deposit':
+							this.ws?.send(
+								JSON.stringify({
+									op: 'unsubscribe',
+									args: [topic]
+								})
+							);
+							this.wsEvents = this.wsEvents.filter(
+								(e) => e !== event
+							);
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		} else {
+			throw new Error('Websocket not connected');
+		}
 	}
 }
